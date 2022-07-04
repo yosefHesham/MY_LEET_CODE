4746f58025ad572e13ae815dabe4b308d53a1f4f
yosefHesham <yousefmobdev@gmail.com>
2022-07-04 13:14:39 +0200

    Delete unnecessary code

--- array_partition.cpp
+++ array_partition.cpp
@@ -31,25 +31,4 @@ using namespace std;
     	cout << arrayPairSum(nums) << endl;
     }
 
-    /* 
-
-    Implementation:
-    sales -> clients -> sign contract (tru mob, truweb ,tru market) -> go to implementaion team (set the product up for the client)
-
-
-
-
-    3 levels of solving a problem
-    1- support engineers (solve internally in support team).
-    2- Solve it in implementation team.
-    3-Developers team.
-    */
-
-
-    /*
-
-    ## True Mobile:
-    (Connects consumers with brokers)
-    : Apply for insurance online
-    : Pin Card, (ro5sa)
-     */
+   
