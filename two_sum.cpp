ebed79a31170476f4693fadf7ecf273e88369824
yosefHesham <youssef.h@trufla.com>
2022-08-27 23:15:37 +0200

    two-sum

---
+++ two_sum.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+ 
+ 
+  vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> indices;
+        for(int i = 0; i < nums.size(); i ++) {
+            indices.insert({nums[i],i});
+        }
+        
+        for(int i = 0; i < nums.size(); i++) {
+        
+        int search = target - nums[i];
+               if( indices.find(search) != indices.end() && i != indices[search])
+                return {i,indices[search]};
+        }
+        
+        return {};
+        
+    }
+   
+int main() {
+  
+
+
+}
